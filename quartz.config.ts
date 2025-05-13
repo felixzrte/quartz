0865586c42bea10bf69ea1a9589fc9d8dedde3b3
felixzrte <felixzarate14@gmail.com>
2025-05-13 10:27:18 -0400

    Build

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Mucklanders",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Josefin Sans",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
